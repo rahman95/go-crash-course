a9e2828037dce4e2a50042434342d81cd2029c80
Rahman Younus <rahman_95@live.co.uk>
2021-03-22 18:31:33 +0000

    added channel example

---
+++ 15-Concurrency/channels/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// See https://gobyexample.com/channels for further info, we will use channels to communicate from and to goroutines
+
+func main() {
+	messages := make(chan string)
+
+	go count("sheep", messages)
+	go count("bird", messages)
+
+	for msg := range messages {
+		fmt.Println(msg)
+	}
+}
+
+func count(thing string, messages chan string) {
+	for i := 0; i <= 5; i++ {
+		messages <- fmt.Sprint(i, " ", thing)
+		time.Sleep(time.Millisecond * 500)
+	}
+
+	close(messages)
+}

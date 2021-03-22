0df39559e0a9b498abd9a8a92f59f00aac02d9ec
Rahman Younus <rahman_95@live.co.uk>
2021-03-22 16:52:08 +0000

    add waitgroup concurrency example

--- 15-Concurrency/goroutine/main.go
+++ 15-Concurrency/goroutine/main.go
@@ -5,20 +5,20 @@ import (
 	"time"
 )
 
-// See https://gobyexample.com/goroutines for further info
+// See https://gobyexample.com/goroutines for further info, below are a few examples of using goroutines
 
 func main() {
 	// 1 - would never count sheep as the main thread will be stuck in the infinite loop counting birds.
-	// count("birds")
-	// count("sheeps")
+	// count("bird")
+	// count("sheep")
 
 	// 2 - using go prefix allows us to use goroutine this will use a seperate thread for execution and the other will run on the main thread
-	go count("birds")
-	count("sheeps")
+	go count("bird")
+	count("sheep")
 
 	// 3 - cant use go prefix for both as that will place both on a seperate thread and since nothing is running on the main it will terminate
-	// go count("birds")
-	// go count("sheeps")
+	// go count("bird")
+	// go count("sheep")
 }
 
 func count(thing string) {

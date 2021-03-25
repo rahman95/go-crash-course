6bbae2a321fce535eb10bd952eea023b31b6f47e
Rahman Younus <rahman_95@live.co.uk>
2021-03-25 15:33:22 +0000

    added fibonacci example

---
+++ 15-Concurrency/fibonacci/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1 - standard fibonacci on main thread
+	// for i := 0; i < 100; i++ {
+	// 	fmt.Println(i, fib(i))
+	// }
+
+	// 2 - fibonacci using workerpools and channels
+	jobs := make(chan int, 100)
+	results := make(chan string, 100)
+
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
+
+	for i := 0; i < 100; i++ {
+		jobs <- i
+	}
+	close(jobs)
+
+	for j := 0; j < 100; j++ {
+		fmt.Println(<-results)
+	}
+}
+
+func worker(jobs <-chan int, results chan<- string) {
+	for n := range jobs {
+		results <- fmt.Sprint(n, fib(n))
+	}
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return fib(n-1) + fib(n-2)
+}
